1c832a1461aa90ea7e938f2543303ddd4407f596
Czz <545155356@qq.com>
2019-06-01 09:35:11 +0800

    First Commit

---
+++ myauction/src/main/java/com/shop/myauction/MyauctionApplication.java
@@ -0,0 +1,13 @@
+package com.shop.myauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyauctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyauctionApplication.class, args);
+    }
+
+}
